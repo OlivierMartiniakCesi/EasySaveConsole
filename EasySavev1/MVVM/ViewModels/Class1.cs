fd7f968ea48ea61216afb1185dd7f1f7ee822b65
Noventy <nathan.bernard@viacesi.fr>
2024-02-08 20:44:32 +0100

    Initialization

---
+++ EasySavev1/MVVM/ViewModels/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySavev1.MVVM.ViewModels
+{
+    class Class1
+    {
+    }
+}
