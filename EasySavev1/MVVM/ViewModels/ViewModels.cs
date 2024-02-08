95cd03d1f37a99422471ea340993f322030d621a
Olivier MARTINIAK <olivier.martiniak@viacesi.fr>
2024-02-08 21:12:40 +0100

    ViewModelFeatures

--- EasySavev1/MVVM/ViewModels/ViewModels.cs
+++ EasySavev1/MVVM/ViewModels/ViewModels.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EasySavev1.MVVM.Models;
+using EasySavev1.MVVM.Views;
 
 namespace EasySavev1.MVVM.ViewModels
 {
     class ViewModels
     {
+        private static Backup _backup = new Backup();
+        private static List<Backup> BackupListInfo = new List<Backup>();
     }
 }
