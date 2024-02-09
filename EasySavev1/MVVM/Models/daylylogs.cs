60514bb9040af886536c9cb7df2466432e0c8eab
LilYanX <108776643+LilYanX@users.noreply.github.com>
2024-02-09 01:47:34 +0100

    [+] Ajout des logs journaliers - Models

---
+++ EasySavev1/MVVM/Models/daylylogs.cs
@@ -0,0 +1,28 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveConsole.Models
+{
+    class daylylogs
+    {
+        public void Logsjson()
+        {
+            // Create directory if it's needed
+            string logDirectory = @"C:\Temp";
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()  // Write logs on console
+            .WriteTo.File(@"C:\Temp\log.json", rollingInterval: RollingInterval.Day) // Write daily logs on JSON File 
+            .CreateLogger();
+        }
+    }
+}
