eaa9ccbebdf7d0fa937db2fff97cfaaba3977618
Lilian MONNAERT <lilian1806@outlook.fr>
2024-02-17 19:09:31 +0100

    [+] Ajout des Models

---
+++ EasySaveV2/MVVM/Models/Backup.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveConsole.MVVM.Models
+{
+    class Backup
+    {
+        private string Name { get; set; }
+        private string SourceDirectory { get; set; }
+        private string TargetDirectory { get; set; }
+        private string Type { get; set; }
+
+        //Constructor
+        public Backup() { }
+
+        //Constructor with parameter
+        public Backup(string Name, string PathSource, string PathTarget, string type)
+        {
+            this.Name = Name;
+            this.SourceDirectory = PathSource;
+            this.TargetDirectory = PathTarget;
+            this.Type = type;
+        }
+
+        ~Backup()
+        {
+            System.Diagnostics.Trace.WriteLine("finalizer is called.");
+        }
+
+        public string getName()
+        {
+            return Name;
+        }
+
+        public void setName(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public string getSourceDirectory()
+        {
+            return SourceDirectory;
+        }
+        public void setSourceDirectory(string SourceDirectory)
+        {
+            this.SourceDirectory = SourceDirectory;
+        }
+
+        public string getTargetDirectory()
+        {
+            return TargetDirectory;
+        }
+
+        public void setTargetDirectory(string TargetDirectory)
+        {
+            this.TargetDirectory = TargetDirectory;
+        }
+
+        public string getType()
+        {
+            return Type;
+        }
+
+        public void setType(string Type)
+        {
+            this.Type = Type;
+        }
+
+        public string getAllInfo()
+        {
+            string Information = "Name : " + Name + "\tSource directory : " + SourceDirectory + "\tTarget directory : " + TargetDirectory + "\tType : " + Type;
+            return Information;
+        }
+
+        public Backup CreateBackup(string Name, string PathSource, string PathTarget, string type)
+        {
+            return new Backup(Name, PathSource, PathTarget, type);
+        }
+
+        public string SaveJson()
+        {
+            string FileStock = "{" +
+             Environment.NewLine + "\t\"Name\":\"" + Name + "\"," +
+             Environment.NewLine + "\t\"Source\":\"" + SourceDirectory + "\"," +
+             Environment.NewLine + "\t\"Target\":\"" + TargetDirectory + "\"," +
+             Environment.NewLine + "\t\"Type\":\"" + Type + "\"" +
+             Environment.NewLine + "}";
+
+            return FileStock.Replace("\\", "\\\\");
+        }
+
+    }
+}
---
+++ EasySaveV2/MVVM/Models/StateLog.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveConsole.MVVM.Models
+{
+    public class StateLog
+    {
+        public string name;
+        public string fileSource;
+        public string fileTarget;
+        public long fileSize;
+        public string state;
+        public long totalFiles;
+        public long nbFilestoGet;
+        public int crypting;
+        public int progression;
+        public string Time;
+
+        public StateLog(string name, string fileSource, string fileTarget, long fileSize, string state, long totalFiles, long totalFilesDone, int crypting)
+        {
+            this.name = name;
+            this.fileSource = fileSource;
+            this.fileTarget = fileTarget;
+            this.totalFiles = totalFiles;
+            this.nbFilestoGet = totalFiles - totalFilesDone;
+            if (this.nbFilestoGet == 0)
+            {
+                this.state = "Off";
+            }
+            else
+            {
+                this.state = "On";
+            }
+            this.fileSize = fileSize;
+            if (totalFilesDone == 0)
+            {
+                this.progression = 0;
+            }
+            else
+            {
+                this.progression = Convert.ToInt32(totalFilesDone / totalFiles) * 100;
+            }
+            this.crypting = crypting;
+            this.Time = DateTime.Now.ToString();
+        }
+    }
+}
