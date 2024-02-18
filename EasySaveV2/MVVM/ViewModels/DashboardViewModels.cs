f0530014684405da44aca68a712ce863a3df5c3f
Lilian MONNAERT <lilian1806@outlook.fr>
2024-02-18 01:02:15 +0100

    [+] dashboard

--- EasySaveV2/MVVM/ViewModels/DashboardViewModels.cs
+++ EasySaveV2/MVVM/ViewModels/DashboardViewModels.cs
@@ -16,11 +16,10 @@ namespace EasySaveV2.MVVM.ViewModels
     {
         public class Backup
         {
-            public string Id { get; set; }
             public string Name { get; set; }
             public string Source { get; set; }
             public string Target { get; set; }
-            public int Type { get; set; }
+            public string Type { get; set; }
         }
     }
 }
