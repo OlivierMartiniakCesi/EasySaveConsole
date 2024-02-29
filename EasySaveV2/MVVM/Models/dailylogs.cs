edbcc8141ef8a9656786f81a8f5669befdd60833
Olivier MARTINIAK <olivier.martiniak@viacesi.fr>
2024-02-29 21:59:18 +0100

    dailylogs

--- EasySaveV2/MVVM/Models/dailylogs.cs
+++ EasySaveV2/MVVM/Models/dailylogs.cs
@@ -10,36 +10,58 @@ namespace EasySaveV2.MVVM.Models
 {
     class dailylogs
     {
+        /***************************************************************/
+        /* Déclaration des attributs en privé pour les logs jounaliers */
+        /***************************************************************/
         private static LoggerConfiguration jsonLoggerConfiguration;
         private static LoggerConfiguration xmlLoggerConfiguration;
-        private static bool isLoggerCreated = false;
-        static string logDirectory = @"C:\Temp";
+        private static string logDirectory = @"C:\Temp";
         private static ILogger jsonLogger;
         private static ILogger xmlLogger;
+
+        /******************************************************************/
+        /* Déclaration des attributs en publique pour les logs jounaliers */
+        /******************************************************************/
         public static ILogger selectedLogger;
 
-        static dailylogs()
+        /*********************************/
+        /* Déclaration d'un constructeur */
+        /*********************************/
+        public dailylogs()
         {
-            // Create directory if it's needed
+            // Création du dossier log
             if (!Directory.Exists(logDirectory))
             {
                 Directory.CreateDirectory(logDirectory);
             }
 
+            // Configurer les logs 
             jsonLoggerConfiguration = new LoggerConfiguration()
                 .WriteTo.File(Path.Combine(logDirectory, "log.json"), rollingInterval: RollingInterval.Day);
 
             xmlLoggerConfiguration = new LoggerConfiguration()
                 .WriteTo.File(Path.Combine(logDirectory, "log.xml"), rollingInterval: RollingInterval.Day);
+
+            // Création d'un logger JSON ou XML
+            // Utiliser pour écrire dans le fichier log
             jsonLogger = jsonLoggerConfiguration.CreateLogger();
             xmlLogger = xmlLoggerConfiguration.CreateLogger();
         }
 
+        /*******************************************************************/
+        /* Déclaration des méthodes en publiques pour les logs journaliers */
+        /*******************************************************************/
+
+        //Méthode pour changer le format des logs
         public void Logsjson(bool logformat)
         {
             string dateFormat = DateTime.Now.ToString("yyyyMMdd");
+
+            // Construit le nom de fichier de log en fonction du format de log
             string logFileName = logformat ? $"log{dateFormat}.xml" : $"log{dateFormat}.json";
             string logFilePath = Path.Combine(logDirectory, logFileName);
+
+            // Sélectionner le format pour les logs
             selectedLogger = logformat ? xmlLogger : jsonLogger;
             selectedLogger.Information("Changement d'extension des logs réalisé");
         }
